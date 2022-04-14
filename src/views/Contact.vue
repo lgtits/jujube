a407a2cf0d200ccfe9fc0eee0cb7c8bb7e227df1
lgtits <a0937370704@gmail.com>
2022-04-14 16:38:13 +0800

    add contact info

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,20 +2,29 @@
   <div class="contact">
     <Header />
     <ComputerHeader />
-
-    <h1>This is a contact page</h1>
-    <div>
-      111年覓棗晨-雪麗(梨)棗訂購專區
-訂單成立匯款後會在3-5個工作天寄出商品,並通知客戶商品已寄出,敬請耐心等候!!
-高雄市阿蓮區農會(代號:619)   匯款帳號:00082220754420  陳炎清先生
-    </div>
-    <div>
-      <img src="../assets/image/table.jpg" alt="">
-    </div>
-    <!-- fb messenger -->
-    <div class="messenger">
-      <div id="fb-root"></div>
-    </div>
+    <main class="contact-info">
+      <!-- <div>
+        111年覓棗晨-雪麗(梨)棗訂購專區
+        訂單成立匯款後會在3-5個工作天寄出商品,並通知客戶商品已寄出,敬請耐心等候!!
+        高雄市阿蓮區農會(代號:619)   匯款帳號:00082220754420  陳炎清先生
+      </div> -->
+      <div class="info">
+        <div class="content phone">
+          <p class="info-title"><font-awesome-icon icon="fa-solid fa-phone" /> 電話客服</p>
+          <p class="info-account">00-0000000</p>
+          <P class="info-time">電話客服時間：星期一～星期五，09:00～18:00</P>
+        </div>
+        <div class="content line">
+          <p class="info-title"><font-awesome-icon icon="fa-brands fa-line" /> LINE 客服</p>
+          <p class="info-account">ID: @XXXXXXX</p>
+          <P class="info-time">LINE 客服時間：星期一～星期五，09:00～18:00</P>
+        </div>
+      </div>
+      <!-- fb messenger -->
+      <div class="messenger">
+        <div id="fb-root"></div>
+      </div>
+    </main>
     <Footer />
   </div>
 </template>
@@ -36,5 +45,24 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .contact-info{
+    padding-top: 60px;
+    margin-bottom: 20px;
+    .info{
+      width: 80%;
+      margin: 0 auto;
+      .content {
+        padding: 16px;
+        border: 1px solid rgb(179, 178, 178);
+        .info-title{
+        font-size: 20px;
+        margin-bottom: 16px;
+        }
+        .info-account{
+          color: $main-green;
+          margin-bottom: 16px;
+        }
+      }
+    }
+  }
 </style>
