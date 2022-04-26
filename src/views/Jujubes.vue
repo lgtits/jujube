8e8cc25010fa88fc08d226816108305be522efa1
lgtits <a0937370704@gmail.com>
2022-04-26 17:03:48 +0800

    cart page initiate

--- src/views/Jujubes.vue
+++ src/views/Jujubes.vue
@@ -6,18 +6,15 @@
       <Sidebar />
       <div class="main-content">
         <h1>
-          棗子
+          棗子 & 棗子糖
         </h1>
         <div class="jujube-box">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard 
+            :initial-jujube="jujube" 
+            v-for="jujube in jujubes"
+            :key="jujube.id"
+          />
         </div>
-        <h1 class="other-product">
-          棗子糖
-        </h1>
-        <ProductCard />
       </div>
     </main>
     <Footer />
@@ -34,20 +31,52 @@ import ProductCard from "../components/productCard.vue";
 const productData = {
   'jujubes': [
     {
-      'name': 'jujube 1',
-      'price': 100
+      'id': 1,
+      'name': '雪麗棗 4.5A',
+      'image':'https://i.imgur.com/gpfAb8E.jpg',
+      'specification':'超大大(每顆平均4.5兩)',
+      'subSpecification':'顆數: 16 ± 1',
+      'price': 750,
+      'quantity': 0
     },
     {
-      'name': 'jujube2',
-      'price': 200
+      'id': 2,
+      'name': '雪麗棗 4A',
+      'image':'https://i.imgur.com/gpfAb8E.jpg',
+      'specification':'超大(每顆平均4.0-4.4兩)',
+      'subSpecification':'顆數: 18 ± 1',
+      'price': 600,
+      'quantity': 0
+    },
+    {
+      'id': 3,
+      'name': '雪麗棗 3.5A',
+      'image':'https://i.imgur.com/gpfAb8E.jpg',
+      'specification':'頂級(每顆平均3.5-3.9兩)',
+      'subSpecification':'顆數: 21 ± 1',
+      'price': 500,
+      'quantity': 0
+    },
+    {
+       'id': 4,
+      'name': '雪麗棗 3A',
+      'image':'https://i.imgur.com/gpfAb8E.jpg',
+      'specification':'特級(每顆平均3.0-3.4兩)',
+      'subSpecification':'顆數: 24 ± 1',
+      'price': 400,
+      'quantity': 0
+    },
+    {
+      'id': 5,
+      'name': '柴燒棗子糖',
+      'image':'https://i.imgur.com/gLE8CD3.jpg',
+      'specification':'頂級蜜棗製作',
+      'subSpecification':'顆數: 10 ± 1',
+      'price': 150,
+      'quantity': 0
+
     }
   ],
-  'others': [
-    {
-      'name': 'jujube candy',
-      'price': 150
-    }
-  ]
 }
 
 export default {
@@ -60,11 +89,13 @@ export default {
   },
   data(){
     return {
-      products: []
+      jujubes: [],
+      others:[]
     }
   },
   created(){
-    this.products = productData.jujubes
+    this.jujubes = productData.jujubes
+    this.others = productData.others
   }
 }
 
